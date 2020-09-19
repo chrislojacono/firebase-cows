1cc47c57dd43054953eba407a972e1afcb9bc691
Chris LoJacono <clojacono92@gmail.com>
2020-09-19 10:48:07 -0500

    firebase stuff

---
+++ src/javascripts/helpers/data/authData.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const checkLoginStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // person is logged in
+      console.log(user);
+      $('#auth').addClass('hide');
+      $('#pasture').removeClass('hide');
+      $('#navbar-logout-button').removeClass('hide');
+      $('#pasture').html(`<img src="${user.photoURL}" alt="${user.displayName}"/>`);
+    } else {
+      $('#navbar-logout-button').addClass('hide');
+      $('#auth').removeClass('hide');
+      $('#pasture').addClass('hide');
+    }
+  });
+};
+
+export default { checkLoginStatus };
